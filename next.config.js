6c97b470cb0f679cfd81bbfc098a76c02ab8966c
Semih Şen <halil.semih.sen@gmail.com>
2025-12-01 16:23:02 +0300

    feat(config): enhance image configuration with remote patterns

--- next.config.js
+++ next.config.js
@@ -3,9 +3,25 @@ const nextConfig = {
   experimental: {
     serverActions: {},
   },
-  eslint:{
-    ignoreDuringBuilds:true
-  }
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
